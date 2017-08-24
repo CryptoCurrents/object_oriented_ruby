2595570f016d0271126e47c8415d10490264d476
JackMarx <seriousfools@gmail.com>
2017-08-23 19:19:22 -0500

    seperate files

--- employee.rb
+++ employee.rb
@@ -1,18 +1,6 @@
-# employee_1 = ["Jimmy", "Stewart", 70000, true]
-# employee_2 = ["Susan", "Messing", 80000, true]
-
-# puts employee_1[0] + " " + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
-# puts "#{employee_2[0]} #{employee_2[1]} makes #{employee_2[2]} a year."
-
-# employee_1 = {:first_name => "Jimmy", :last_name => "Stewart", :salary => 70000, :active => true}
-# employee_2 = {first_name: "Susan", last_name: "Messing", salary: 80000, active: true}
-
-# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
-# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
-
 class Employee
   attr_reader :first_name, :last_name, :active
-  attr_writer :active, :salary
+  attr_writer :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
@@ -22,51 +10,10 @@ class Employee
   end
 
   def print_info
-    puts "#{first_name} #{last_name} makes #{@salary} a year."
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
   end
 
   def give_annual_raise
-    @salary = @salary * 1.05
+    @salary = 1.05 * @salary
   end
 end
-
-employee_1 = Employee.new(
-                          first_name: "Jimmy", 
-                          last_name: "Stewart", 
-                          salary: 70000, 
-                          active: true
-                          )
-
-employee_2 = Employee.new(
-                          first_name: "Susan", 
-                          last_name: "Messing", 
-                          salary: 80000, 
-                          active: true
-                          )
-
-class Manager < Employee
-  def initialize(input_options)
-    super(input_options)
-    @employees = input_options[:employees]  
-  end
-
-  def send_report
-    puts "Sending Email..."
-    # code to send the email
-    puts "Email Sent."
-  end
-end
-
-manager = Manager.new(
-                      first_name: "Bob",
-                      last_name: "Hoskins",
-                      salary: 100000,
-                      active: true,
-                      employees: [employee_1, employee_2]
-                      )
-
-
-
-
-
-
